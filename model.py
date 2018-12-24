84a423158c6b60e77a6f763877052b42305b7f97
Vikash <vikash@programminggeek.in>
2018-12-24 12:07:24 +0000

    Add working model

--- model.py
+++ model.py
@@ -36,7 +36,7 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 steering_center = float(batch_sample[3])
 
-                correction = 0.1 # this is a parameter to tune
+                correction = 0.2 # this is a parameter to tune
                 steering_left = steering_center + correction
                 steering_right = steering_center - correction
 
@@ -89,17 +89,20 @@ model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 20), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dropout(0.5))
 model.add(Dense(50))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(10))
-model.add(Dropout(0.5))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= int(len(train_samples)/batch_size), 
